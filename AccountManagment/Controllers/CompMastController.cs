0889161bd7ed98645e8a1cdca66bd878638b0d68
nikhil <nikhil2545sangani@gmail.com>
2025-04-01 14:37:59 +0530

    authorization added in controller

--- AccountManagment/Controllers/CompMastController.cs
+++ AccountManagment/Controllers/CompMastController.cs
@@ -1,12 +1,14 @@
 ﻿using Acc.Services.Common;
 using Acc.Services.Interfaces;
 using Acc.Shared.Dtos;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 
 namespace Acc.Api.Controllers
 {
+	[Authorize]
 	[Route("api/[controller]")]
 	[ApiController]
 	public class CompMastController : ControllerBase
--- AccountManagment/Controllers/ShpMastController.cs
+++ AccountManagment/Controllers/ShpMastController.cs
@@ -2,12 +2,14 @@
 using Acc.Services.Interfaces;
 using Acc.Services.Services;
 using Acc.Shared.Dtos;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 
 namespace Acc.Api.Controllers
 {
+	[Authorize]
 	[Route("api/[controller]")]
 	[ApiController]
 	public class ShpMastController : ControllerBase
